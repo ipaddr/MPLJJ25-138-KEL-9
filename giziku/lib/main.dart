d86c1153b653a6855bd459cce5990d82aafdaed1
Alvin Anugerah Pratama <116614563+Verruct51@users.noreply.github.com>
2025-06-12 22:13:00 +0700

    a

--- giziku/lib/main.dart
+++ giziku/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // inisialisasi Firebase
   runApp(const GiziKuApp());
 }
 
